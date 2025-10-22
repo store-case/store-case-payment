130ee433021b36d759b5d9c7b501c0a5adcc5f5c
honee8583 <ekgns0508@gmail.com>
2025-10-22 14:55:01 +0900

    chore: init project

---
+++ src/main/java/com/leedahun/storecasepayment/StoreCasePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.leedahun.storecasepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCasePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCasePaymentApplication.class, args);
+    }
+
+}
